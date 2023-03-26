9735b8120d2ee6cfe2c5dc3c6c0c6b7a18d3b7bb
Bella <georgia.bajanescu02@e-uvt.ro>
2023-03-26 16:20:09 +0300

    Initial commit

---
+++ src/main/java/com/internship/internflow/InternFlowApplication.java
@@ -0,0 +1,14 @@
+package com.internship.internflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternFlowApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello intern! :)");
+		SpringApplication.run(InternFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/internflow/InternFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.internflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
