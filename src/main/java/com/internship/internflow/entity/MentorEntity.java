311e183628b10474b723e48f4073cf281ca84077
BiancaBarbaliu <bianca.barbaliu02@e-uvt.ro>
2023-03-28 19:00:19 +0300

    Add MentorEntity class - rel

--- src/main/java/com/internship/internflow/entity/MentorEntity.java
+++ src/main/java/com/internship/internflow/entity/MentorEntity.java
@@ -1,4 +1,83 @@
 package com.internship.internflow.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "Mentor")
 public class MentorEntity {
+    @Id
+    @SequenceGenerator(
+            name = "studentGenerator",
+            sequenceName = "intern_flow.sq_student_id",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mentorGenerator")
+    @Column(name = "mentor_id")
+    private Long mentorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "registration_date")
+    private OffsetDateTime timestamp;
+
+    public MentorEntity() {
+        //   no-arg constructor
+    }
+
+    // Getters and setters
+    public Long getMentorId() {
+        return mentorId;
+    }
+
+    public void setMentorId(Long mentorId) {
+        this.mentorId = mentorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
