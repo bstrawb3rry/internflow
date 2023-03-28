7d78fb44407fb51a9392c294a9eae95a89040a14
Bella <georgia.bajanescu02@e-uvt.ro>
2023-03-28 18:53:29 +0300

    Add student entity class - rel

--- src/main/java/com/internship/internflow/entity/StudentEntity.java
+++ src/main/java/com/internship/internflow/entity/StudentEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
+import java.time.OffsetDateTime;
+
 @Entity
 @Table(name = "Student")
 
@@ -19,23 +21,67 @@ public class StudentEntity {
             sequenceName = "intern_flow.sq_student_id",
             allocationSize = 1
     )
-
+    @Column(name = "student_id")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "studentGenerator")
-    private Long student_id;
+    private Long studentId;
 
     @Column(name = "first_name")
     private String firstName;
 
-    @Column
-    private String last_name;
+    @Column(name = "last_name")
+    private String lastName;
 
-    @Column
+    @Column(name = "email")
     private String email;
 
-    @Column
-    private String description;
+    @Column(name = "registration_date")
+    private OffsetDateTime timestamp;
 
-    @Column
-    private Integer age;
+    public StudentEntity() {
+        // no-arg constructor
+    }
+
+    // Getters and Setters
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
+
+
 
