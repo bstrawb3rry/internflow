99b78646b8a8e0a939425bdcc5924ae1119a4ae0
Raluca <raluca.osman02@e-uvt.ro>
2023-03-28 19:13:24 +0300

    Add activities entity - rel

--- src/main/java/com/internship/internflow/entity/ActivitiesEntity.java
+++ src/main/java/com/internship/internflow/entity/ActivitiesEntity.java
@@ -1,4 +1,56 @@
 package com.internship.internflow.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Activities")
 public class ActivitiesEntity {
+    @Id
+    @SequenceGenerator(
+            name = "activitiesGenerator",
+            sequenceName = "intern_flow.sq_activity_id",
+            allocationSize = 1
+    )
+    @Column(name = "activity_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "activitiesGenerator")
+    private Long activityId;
+
+    @Column(name = "activity_name")
+    private String activityName;
+
+    public ActivitiesEntity() {
+        //no-arg Constructor
+    }
+
+
+    // Getters and Setters
+
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
 }
